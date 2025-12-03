dcd17c3ddc126a440956435eb452b996617a9b38
Sezar <julio.sezar@gmail.com>
2025-12-04 01:02:32 +0330

    feat: add user model, first db migration, add important settings

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    # We don't need to add anything here. 
+    # AbstractUser already enforces Username and Password login.
+    pass
